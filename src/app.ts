c43e5b3dcaf18c7c01f90a54d925475ab859a898
Ian Walters <ian.walters@roames.com.au>
2020-04-22 11:43:34 +1000

    make its own stack

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'source-map-support';
 import * as Core from '@aws-cdk/core';
 import * as Static from '@elhedranorg/cdk-static-site';
+import * as path from 'path';
 
 const app = new Core.App();
 const siteName = 'devopssample';
@@ -12,12 +13,13 @@ class MainStack extends Core.Stack {
         new Static.StaticSite(this, 'site', {
             zoneDomain: 'elhedran.com',
             siteDomain: 'devopssample.elhedran.com',
-            siteName
+            siteName,
+            assetPath: path.resolve('assets', 'site')
         });
     }
 }
 
-new MainStack(app, 'sample-stack', {
+new MainStack(app, 'devopssample', {
     env: {
         account: '057191276549',
         region: 'us-east-1'
